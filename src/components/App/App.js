eb5edd184bc7e02a9d76cb34a9b4cd424abd54eb
Никита <nik.loktev151069@gmail.com>
2022-12-07 10:31:33 +0300

    Commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import TicketList from "../TicketList";
+import Filter from "../Filter";
+
+function App() {
+  return (
+    <div className="aviasales-app">
+      <Filter />
+      <TicketList />
+    </div>
+  );
+}
+
+export default App;
